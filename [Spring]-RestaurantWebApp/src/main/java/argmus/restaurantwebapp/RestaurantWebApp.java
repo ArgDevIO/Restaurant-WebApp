aa5d1b803734e22d35440e9cb1ecfef036b8f6a2
Argetim Ramadani <argdev@outlook.com>
2020-04-04 22:57:39 +0200

    Initial commit

---
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/RestaurantWebApp.java
@@ -0,0 +1,13 @@
+package argmus.restaurantwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantWebApp.class, args);
+    }
+
+}
