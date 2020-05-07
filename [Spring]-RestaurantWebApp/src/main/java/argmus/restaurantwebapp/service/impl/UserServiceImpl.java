b36970589eb71e7a62ce38f312b8a4a82869bebc
Argetim Ramadani <argdev@outlook.com>
2020-05-08 01:15:35 +0200

    Fix UserId not saving on Address relation

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/service/impl/UserServiceImpl.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/service/impl/UserServiceImpl.java
@@ -1,19 +1,25 @@
 package argmus.restaurantwebapp.service.impl;
 
+import argmus.restaurantwebapp.model.Address;
 import argmus.restaurantwebapp.model.User;
+import argmus.restaurantwebapp.repository.AddressRepository;
 import argmus.restaurantwebapp.repository.UserRepository;
 import argmus.restaurantwebapp.service.UserService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, AddressRepository addressRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.addressRepository = addressRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
@@ -21,9 +27,16 @@ public class UserServiceImpl implements UserService {
     public User saveUser(User newUser) {
         newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
+        Address primaryAddress = newUser.getAddresses().iterator().next();
+        newUser.setAddresses(new HashSet<>());
+
+        User savedUser = this.userRepository.save(newUser);
+        primaryAddress.setUser(savedUser);
+        this.addressRepository.save(primaryAddress);
+
         // email has to be unique (exception)
         // make sure that password & confirm password match
         // we don't persist or show the confirmPassword
-        return this.userRepository.save(newUser);
+        return savedUser;
     }
 }
