6af5516b7ff402c5927367730810d6b7d40525ef
Argetim Ramadani <argdev@outlook.com>
2020-05-07 23:19:56 +0200

    Implement save/create User on User service

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/service/UserService.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/service/UserService.java
@@ -1,4 +1,9 @@
 package argmus.restaurantwebapp.service;
 
+import argmus.restaurantwebapp.model.Address;
+import argmus.restaurantwebapp.model.User;
+
 public interface UserService {
+
+    User saveUser(String fullName, String email, String password, String confirmPassword, String phone, Address primaryAddress);
 }
--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/service/impl/UserServiceImpl.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/service/impl/UserServiceImpl.java
@@ -1,15 +1,30 @@
 package argmus.restaurantwebapp.service.impl;
 
+import argmus.restaurantwebapp.model.Address;
+import argmus.restaurantwebapp.model.User;
 import argmus.restaurantwebapp.repository.UserRepository;
 import argmus.restaurantwebapp.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public User saveUser(String fullName, String email, String password, String confirmPassword, String phone, Address primaryAddress) {
+        User newUser = new User(fullName, email, bCryptPasswordEncoder.encode(password), bCryptPasswordEncoder.encode(confirmPassword), phone, primaryAddress);
+
+        // email has to be unique (exception)
+        // make sure that password & confirm password match
+        // we don't persist or show the confirmPassword
+        return this.userRepository.save(newUser);
     }
 }
