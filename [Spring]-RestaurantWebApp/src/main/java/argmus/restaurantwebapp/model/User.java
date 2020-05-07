cdc79f6940ded7483579b0b5dcaa81ec14bff0e0
Argetim Ramadani <argdev@outlook.com>
2020-05-07 19:15:20 +0200

    Create User model

---
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/model/User.java
@@ -0,0 +1,47 @@
+package argmus.restaurantwebapp.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @Email(message = "Invalid email")
+    @NotBlank(message = "email is required")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Transient
+    private String confirmPassword;
+
+    private Date created_At;
+    private Date updated_At;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
+
+}
