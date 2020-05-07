29999356c3d4abdd213b45e9b3ccdce7e6e0d73b
Argetim Ramadani <argdev@outlook.com>
2020-05-07 23:20:18 +0200

    Create BCryptPasswordEncoder Bean

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/security/SecurityConfig.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package argmus.restaurantwebapp.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -31,5 +33,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .anyRequest().authenticated();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
