12b7834a254eb04d27b5930af886d0f758b5a764
Argetim Ramadani <argdev@outlook.com>
2020-05-08 20:39:32 +0200

    Fix User getUsername()

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/model/User.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/model/User.java
@@ -71,7 +71,7 @@ public class User implements UserDetails {
     @JsonIgnore
     @Override
     public String getUsername() {
-        return null;
+        return this.email;
     }
 
     @JsonIgnore
