45563dde3aba64918fdae5c7d8e91fed066badeb
Argetim Ramadani <argdev@outlook.com>
2020-05-08 20:40:19 +0200

    Add 'findUserByEmail' method to UserRepo interface

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/repository/UserRepository.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/repository/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     boolean existsUserByEmail(String email);
+    User findUserByEmail(String email);
 }
