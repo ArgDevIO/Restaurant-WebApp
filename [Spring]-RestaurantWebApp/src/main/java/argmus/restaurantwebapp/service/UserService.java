3d7b5fc8433d0743a4da4c2e1844d8ab460cdc03
Argetim Ramadani <argdev@outlook.com>
2020-05-08 18:43:20 +0200

    Add 'newAddressToUser' & 'getUserById' methods to UserService interface

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/service/UserService.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/service/UserService.java
@@ -1,8 +1,13 @@
 package argmus.restaurantwebapp.service;
 
+import argmus.restaurantwebapp.model.Address;
 import argmus.restaurantwebapp.model.User;
 
 public interface UserService {
 
     User saveUser(User newUser);
+
+    User getUserById(Long userId);
+
+    Address newAddressToUser(Long userId, Address address);
 }
