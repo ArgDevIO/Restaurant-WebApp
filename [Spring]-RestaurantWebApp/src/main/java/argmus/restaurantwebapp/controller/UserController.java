e202cd7a99df1ed7bcab46060fe29357fda068c9
Argetim Ramadani <argdev@outlook.com>
2020-05-08 22:52:25 +0200

    Implement login endpoint on User controller

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/controller/UserController.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/controller/UserController.java
@@ -2,16 +2,25 @@ package argmus.restaurantwebapp.controller;
 
 import argmus.restaurantwebapp.model.Address;
 import argmus.restaurantwebapp.model.User;
+import argmus.restaurantwebapp.payload.JWTLoginSuccessResponse;
+import argmus.restaurantwebapp.payload.LoginRequest;
+import argmus.restaurantwebapp.security.JwtTokenProvider;
 import argmus.restaurantwebapp.service.MapValidationErrorService;
 import argmus.restaurantwebapp.service.UserService;
 import argmus.restaurantwebapp.validator.UserValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static argmus.restaurantwebapp.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping(value = "/user", produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
@@ -21,11 +30,33 @@ public class UserController {
     private final UserService userService;
     private final UserValidator userValidator;
     private final MapValidationErrorService mapValidationErrorService;
+    private final JwtTokenProvider tokenProvider;
+    private final AuthenticationManager authenticationManager;
 
-    public UserController(UserService userService, UserValidator userValidator, MapValidationErrorService mapValidationErrorService) {
+    public UserController(UserService userService, UserValidator userValidator, MapValidationErrorService mapValidationErrorService, JwtTokenProvider tokenProvider, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.userValidator = userValidator;
         this.mapValidationErrorService = mapValidationErrorService;
+        this.tokenProvider = tokenProvider;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = this.mapValidationErrorService.MapValidationError(result);
+        if (errorMap != null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
     }
 
     @PostMapping("/register")
