f6b5759cfe7ed0b0174ea2a55017afbee5de0cd6
Argetim Ramadani <argdev@outlook.com>
2020-05-09 18:46:07 +0200

    Update SecurityConstants.java

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/security/SecurityConstants.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/security/SecurityConstants.java
@@ -3,8 +3,9 @@ package argmus.restaurantwebapp.security;
 public class SecurityConstants {
 
     public static final String USER_REGISTER_URLS = "/user/**";
+    public static final String PRODUCTS_GET_URLS = "/menu/product";
     public static final String SECRET = "ArgDevIO-mustran";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 30_000; //30 seconds
+    public static final long EXPIRATION_TIME = 30 * 60_000; //30 minutes
 }
