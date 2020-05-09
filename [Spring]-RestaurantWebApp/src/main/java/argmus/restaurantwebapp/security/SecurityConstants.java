0c27bb9afbf1fe57c38bf7595bbffa983ce82fb7
Argetim Ramadani <argdev@outlook.com>
2020-05-09 21:47:19 +0200

    Update SecurityConstants.java

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/security/SecurityConstants.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/security/SecurityConstants.java
@@ -2,8 +2,6 @@ package argmus.restaurantwebapp.security;
 
 public class SecurityConstants {
 
-    public static final String USER_REGISTER_URLS = "/user/**";
-    public static final String PRODUCTS_GET_URLS = "/menu/product";
     public static final String SECRET = "ArgDevIO-mustran";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
