d2dc22833747c791785bfb7f7c8f082d9569925f
Argetim Ramadani <argdev@outlook.com>
2020-05-10 01:21:18 +0200

    Implement SSL/HTTPs redirection with self signed certificate

--- [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/RestaurantWebApp.java
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/RestaurantWebApp.java
@@ -1,7 +1,14 @@
 package argmus.restaurantwebapp;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestaurantWebApp {
@@ -10,4 +17,32 @@ public class RestaurantWebApp {
         SpringApplication.run(RestaurantWebApp.class, args);
     }
 
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Traffic
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPs redirect
+        serverFactory.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+        return serverFactory;
+    }
+
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 }
