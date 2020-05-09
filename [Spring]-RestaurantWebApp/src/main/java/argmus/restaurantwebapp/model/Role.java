27cdaab4b30b8a302c7ef6225fe63d95ff1b61c8
Argetim Ramadani <argdev@outlook.com>
2020-05-09 18:45:19 +0200

    Create Role model

---
+++ [Spring]-RestaurantWebApp/src/main/java/argmus/restaurantwebapp/model/Role.java
@@ -0,0 +1,48 @@
+package argmus.restaurantwebapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Role name is required")
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+
+        Role role = (Role) o;
+
+        if (getId() != null ? !getId().equals(role.getId()) : role.getId() != null) return false;
+        if (getName() != null ? !getName().equals(role.getName()) : role.getName() != null) return false;
+        return getUsers() != null ? getUsers().equals(role.getUsers()) : role.getUsers() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getUsers() != null ? getUsers().hashCode() : 0);
+        return result;
+    }
+}
