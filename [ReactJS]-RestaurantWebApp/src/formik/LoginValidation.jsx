ce466f1317fb200c911f8ee2d9c01d5092ccc05e
mustran <musli.ademi@inplayer.com>
2020-05-14 23:39:44 +0200

    Change email error text

--- [ReactJS]-RestaurantWebApp/src/formik/LoginValidation.jsx
+++ [ReactJS]-RestaurantWebApp/src/formik/LoginValidation.jsx
@@ -2,7 +2,7 @@ import { object, string } from 'yup';
 
 export const logInValidationSchema = object({
     email: string()
-        .email('Wrong email')
+        .email('Invalid email')
         .required('Required'),
     password: string()
         .min(5, 'Password is too short - should be 5 chars minimum.')
