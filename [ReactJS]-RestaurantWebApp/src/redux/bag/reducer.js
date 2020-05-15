9111db4eaae4488076d7a80f3fe159437ef28ed7
mustran <musli.ademi@inplayer.com>
2020-05-15 23:35:03 +0200

    Add current quantity and get display it in numeric input

--- [ReactJS]-RestaurantWebApp/src/redux/bag/reducer.js
+++ [ReactJS]-RestaurantWebApp/src/redux/bag/reducer.js
@@ -12,9 +12,11 @@ const initialState = {
 const bagReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_PRODUCT_TO_BAG:
+            const productToBeAdded = action.payload;
+            if (!productToBeAdded.currentQuantity) productToBeAdded.currentQuantity = 1;
             return {
                 ...state,
-                bagProducts: [...state.bagProducts, action.payload],
+                bagProducts: [...state.bagProducts, productToBeAdded],
             };
         case REMOVE_PRODUCT_FROM_BAG:
             return {
@@ -28,6 +30,7 @@ const bagReducer = (state = initialState, action) => {
             const updatedProductsIncrease = state.bagProducts.map((product) => {
                 if (product.name === action.payload.name) {
                     product.quantity += 1;
+                    product.currentQuantity += 1;
                     product.totalPrice = product.quantity * product.price;
                 }
                 return product;
