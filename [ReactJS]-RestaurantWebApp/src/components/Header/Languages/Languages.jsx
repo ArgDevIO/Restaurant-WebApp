c5269f8d2f4c6d7f0c4002b76e0b380b45aecede
mustran <musli.ademi@inplayer.com>
2020-05-16 01:52:53 +0200

    Prettier format

--- [ReactJS]-RestaurantWebApp/src/components/Header/Languages/Languages.jsx
+++ [ReactJS]-RestaurantWebApp/src/components/Header/Languages/Languages.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import uuid  from 'uuid/dist/v4';
+import uuid from 'uuid/dist/v4';
 import Language from './Language';
 
 const Languages = ({ languages }) => {
-    return languages.map((language) => (
-        <Language key={uuid()} language={language.language} />
-    ));
+    return languages.map((language) => <Language key={uuid()} language={language.language} />);
 };
 
 export default Languages;
