7f2b51738e72bfeca4a86479d790849d287c50a8
mustran <musli.ademi@inplayer.com>
2020-05-15 23:11:50 +0200

    Validate password using regex pattern

--- [ReactJS]-RestaurantWebApp/src/formik/RegisterValidation.jsx
+++ [ReactJS]-RestaurantWebApp/src/formik/RegisterValidation.jsx
@@ -7,9 +7,7 @@ const passwordPattern = /(?=\D*\d)\S{8,}$/;
 export const registerValidationSchema = object({
     fullName: string().min(5, 'Too short - should be 5 chars minimum').required('Required'),
     email: string().email('Invalid email').required('Required'),
-    password: string()
-        .min(5, 'Password is too short - should be 5 chars minimum.')
-        .required('Required'),
+    password: string().matches(passwordPattern).required('Required'),
     confirmPassword: string()
         .oneOf([ref('password')], 'Both password need to be the same')
         .required('Required'),
