df075a8650aac35dde3b7883727099e15fd92ed2
mustran <musli.ademi@inplayer.com>
2020-05-15 23:47:57 +0200

    FIx register validation

--- [ReactJS]-RestaurantWebApp/src/formik/RegisterValidation.jsx
+++ [ReactJS]-RestaurantWebApp/src/formik/RegisterValidation.jsx
@@ -7,14 +7,16 @@ const passwordPattern = /(?=\D*\d)\S{8,}$/;
 export const registerValidationSchema = object({
     fullName: string().min(5, 'Too short - should be 5 chars minimum').required('Required'),
     email: string().email('Invalid email').required('Required'),
-    password: string().matches(passwordPattern).required('Required'),
+    password: string()
+        .matches(passwordPattern, 'Password must contain at least 8 characters and 1 number')
+        .required('Required'),
     confirmPassword: string()
         .oneOf([ref('password')], 'Both password need to be the same')
         .required('Required'),
     phone: string().matches(phoneNumberPattern, 'Invalid phone number').required('Required'),
     name: string().required('Required'),
     street: string().required('Required'),
-    number: number(),
+    number: number().required("Required"),
     entry: string().nullable(),
     appartment: string().nullable(),
     interphoneCode: string().nullable(),
