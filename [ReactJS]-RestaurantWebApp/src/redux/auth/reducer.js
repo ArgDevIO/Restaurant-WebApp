2682988c08b7e8c30656544c44896bb82e04a9b7
mustran <musli.ademi@inplayer.com>
2020-05-17 19:04:08 +0200

    DIsplay notification on failed login attempt

--- [ReactJS]-RestaurantWebApp/src/redux/auth/reducer.js
+++ [ReactJS]-RestaurantWebApp/src/redux/auth/reducer.js
@@ -1,11 +1,14 @@
 import { FETCH_DATA_ERROR, FETCH_DATA_PRELOAD, FETCH_DATA_SUCCESS } from './actions';
 import jwt from 'jwt-decode';
+import { NOTIFICATION_DISPLAY_REQUEST } from '../../middleware/notifications/actions';
 
 export const initialState = {
     preload: false,
     user: '',
     token: [],
     isLoggedIn: false,
+    errorNotification: false,
+    errorMessage: '',
 };
 
 const authReducer = (state = initialState, action) => {
@@ -23,11 +26,19 @@ const authReducer = (state = initialState, action) => {
                 token: action.payload.token,
                 preload: false,
                 isLoggedIn: true,
+                errorNotification: false,
+                errorMessage: '',
             };
         case FETCH_DATA_ERROR:
             return {
                 ...state,
                 preload: false,
+            };
+        case NOTIFICATION_DISPLAY_REQUEST:
+            return {
+                ...state,
+                errorNotification: true,
+                errorMessage: action.payload,
             };
         default:
             return state;
@@ -38,5 +49,7 @@ export const getUser = (state) => state.auth.user;
 export const getToken = (state) => state.auth.token;
 export const getPreload = (state) => state.auth.preload;
 export const getIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const getErrorNotification = (state) => state.auth.errorNotification;
+export const getErrorMessage = (state) => state.auth.errorMessage;
 
 export default authReducer;
