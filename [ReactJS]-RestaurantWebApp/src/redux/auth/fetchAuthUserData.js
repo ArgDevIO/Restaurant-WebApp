91e6d8470c34f2e13198e313532740734b1868cf
mustran <musli.ademi@inplayer.com>
2020-05-17 19:03:50 +0200

    Dispatch notifRequest if login is not successful

--- [ReactJS]-RestaurantWebApp/src/redux/auth/fetchAuthUserData.js
+++ [ReactJS]-RestaurantWebApp/src/redux/auth/fetchAuthUserData.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import { fetchDataError, fetchDataPreload, fetchDataSuccess } from './actions';
+import { notifRequested } from '../../middleware/notifications/actions';
 
 const url = 'http://localhost:8080/api/users/login';
 
 export const fetchAuthUserData = (email, password) => {
+    console.log('ENTER HERE');
+    console.log(email, password);
     const loginUserCredentials = {
         email,
         password,
@@ -19,6 +22,14 @@ export const fetchAuthUserData = (email, password) => {
             localStorage.setItem('token', data.token);
             return response;
         } catch (e) {
+            console.log(e.response.data)
+            dispatch(
+                notifRequested({
+                    title: 'ERROR',
+                    type: 'danger',
+                    content: e.response.data,
+                })
+            );
             dispatch(fetchDataError());
             // show login error
             return false;
