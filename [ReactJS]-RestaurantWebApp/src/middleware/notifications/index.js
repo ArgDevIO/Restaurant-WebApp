b7827191e6fd086562f56809ca0b2ce634aa072d
mustran <musli.ademi@inplayer.com>
2020-05-19 00:44:58 +0200

    Add notification middleware

---
+++ [ReactJS]-RestaurantWebApp/src/middleware/notifications/index.js
@@ -0,0 +1,17 @@
+import Notification from '../../components/Notification/Notification';
+import { NOTIFICATION_DISPLAY_REQUEST } from './actions';
+
+export default (store) => (next) => (action) => {
+    if (action.type !== NOTIFICATION_DISPLAY_REQUEST) {
+        return next(action);
+    }
+
+    const { type, content, title } = action.payload;
+
+    Notification.create({
+        variant: type,
+        title,
+        content: content.data ? content.data : content,
+        duration: 3,
+    });
+};
