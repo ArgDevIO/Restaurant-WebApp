02f19aff5ac09f667a3e0fb14dc404b34da4b07c
Argetim Ramadani <argdev@outlook.com>
2020-06-12 20:59:12 +0200

    Update data.sql

--- [Spring]-RestaurantWebApp/src/main/resources/data.sql
+++ [Spring]-RestaurantWebApp/src/main/resources/data.sql
@@ -18,9 +18,13 @@ VALUES
 (2, 1, 'Test, Test, Test', 'Test1', 120, 1),
 (3, 1, '2 Eggs, Olives', 'Omlette', 130, 1),
 (4, 1, 'Test, Test, Test', 'Test2', 120, 1),
-(5, 1, 'Test, Test, Test', 'Test3', 150, 2),
-(6, 1, 'Test, Test, Test', 'Test4', 120, 2),
-(7, 1, 'Test, Test, Test', 'Test5', 120, 3),
-(8, 1, 'Test, Test, Test', 'Test6', 150, 3),
-(9, 1, 'Test, Test, Test', 'Test7', 120, 3);
+(5, 1, 'Test, Test, Test', 'Test3', 120, 1),
+(6, 1, 'Test, Test, Test', 'Test4', 120, 1),
+(7, 1, 'Test, Test, Test', 'Test5', 120, 1),
+(8, 1, 'Test, Test, Test', 'Test6', 120, 1),
+(9, 1, 'Test, Test, Test', 'Test7', 150, 2),
+(10, 1, 'Test, Test, Test', 'Test8', 120, 2),
+(11, 1, 'Test, Test, Test', 'Test9', 120, 3),
+(12, 1, 'Test, Test, Test', 'Test10', 150, 3),
+(13, 1, 'Test, Test, Test', 'Test11', 120, 3);
 
